873d8dfd21615524dd52ca04c0a422d8ad802499
PhucNK1 <nguyenkhacphuc311@gmail.com>
2024-07-14 21:03:40 +0700

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        //implement here
+    }
+//    git remote add origin https://github.com/NguyenKhacPhuc/bank-simulator.git
+//    git branch -M master
+//    git push -u origin master
+}
