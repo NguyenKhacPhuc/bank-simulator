79f574e0eb2586193274959b348bc6459846570d
quandha <quandha07@gmail.com>
2024-07-19 20:27:30 +0700

    "delete the PHONE field"

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -2,14 +2,12 @@ public class Customer {
     private String customerID;
     private String Name;
     private String Address;
-    private String Phone;
     private String accountsID;
 
-    public Customer(String customerID, String name, String address, String phone, String accountsID) {
+    public Customer(String customerID, String name, String address, String accountsID) {
         this.customerID = customerID;
         Name = name;
         Address = address;
-        Phone = phone;
         this.accountsID = accountsID;
     }
 
@@ -35,14 +33,6 @@ public class Customer {
 
     public void setAddress(String address) {
         Address = address;
-    }
-
-    public String getPhone() {
-        return Phone;
-    }
-
-    public void setPhone(String phone) {
-        Phone = phone;
     }
 
     public String getAccountsID() {
