51530da7d659f269312a57f7cd1844edbe2f6eb9
Vinhthuba <100991117+Vinhthuba@users.noreply.github.com>
2024-07-21 16:51:06 +0700

    done

--- src/main/java/Bank/Bank.java
+++ src/main/java/Bank/Bank.java
@@ -1,36 +1,52 @@
 package Bank;
 
+import java.util.ArrayList;
 import java.util.List;
-
 public class Bank {
     private List<Customer> customers;
     private List<Account> accounts;
-
+    private List<Transaction> transactions;
+    
+    public Bank() {
+        customers = new ArrayList<>();
+        accounts = new ArrayList<>();
+        transactions = new ArrayList<>();
+    }
+    
     public void addCustomer(Customer customer) {
         customers.add(customer);
     }
-
+    
     public void addAccount(Account account) {
         accounts.add(account);
     }
-
-    public void removeAccount(String accountId) {
-        accounts.removeIf(account -> account.getAccountId().equals(accountId));
-    }
-
-    public void executeTransaction(Transaction transaction) {
-        // Transaction logic
-    }
-
-    public Customer getCustomerDetails(String customerId) {
-        return customers.stream().filter(c -> c.getCustomerId().equals(customerId)).findFirst().orElse(null);
-    }
-
-    public Account getAccountDetails(String accountId) {
-        return accounts.stream().filter(a -> a.getAccountId().equals(accountId)).findFirst().orElse(null);
+    
+    public void removeAccount(Account account) {
+        accounts.remove(account);
     }
     
-    // Additional Methods
+    public void executeTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+    
+    public Customer getCustomerDetails(String customerId) {
+        for(Customer customer : customers) {
+            if(customer.getCustomerID().equals(customerId)) {
+                return customer;
+            }
+        }
+        return null;
+    }
+    
+    public Account getAccountDetails(int accountId) {
+        for(Account account : accounts) {
+            if(account. getAccountID()== accountId) {
+                return account;
+            }
+        }
+        return null;
+    }
 }
+
 
 
