8840e809f023e830e76a9684d9b3522540dd7a06
Nda26031 <nda26031@gmail.com>
2024-07-23 01:32:58 +0700

    Update Delete Account(id, name) and Customer (by id)

---
+++ src/main/java/Customer.java
@@ -0,0 +1,49 @@
+public class Customer {
+    private int customerID;
+    private String Name;
+    private String Address;
+    private String accountsID;
+
+    public Customer(int customerID, String name, String address, String accountsID) {
+        this.customerID = customerID;
+        Name = name;
+        Address = address;
+        this.accountsID = accountsID;
+    }
+
+    public Customer() {
+
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getAccountsID() {
+        return accountsID;
+    }
+
+    public void setAccountsID(String accountsID) {
+        this.accountsID = accountsID;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,22 +8,28 @@ public class Main {
         final String USER_NAME = "root";
 
         Transaction transactionDB = new Transaction();
-        transactionDB.setTransactionId(3);
+        Customer customerDB = new Customer();
+        Account accountDB = new Account();
 
         try {
             Connection conn = getConnection(DB_URL, USER_NAME, PASSWORD);
             Statement statement = conn.createStatement();
 
             System.out.println("Enter your choice");
-            System.out.println("1. Delete with Transaction ID");
+            System.out.println("1. Delete Transaction");
+            System.out.println("2. Delete Customer");
+            System.out.println("3. Delete Account");
 
-            int choice = new Scanner(System.in).nextInt();
-
-            switch (choice) {
+            switch (new Scanner(System.in).nextInt()) {
                 case 1:
-                delete(conn,transactionDB );
+                    deleteTransaction(conn, transactionDB);
                     break;
-
+                case 2:
+                    deleteCustomer(conn, customerDB);
+                    break;
+                case 3:
+                    deleteAccount(conn, accountDB);
+                    break;
                 default:
                     break;
             }
@@ -45,13 +51,59 @@ public class Main {
         return conn;
     }
 
-    public static void delete(Connection conn, Transaction trans) throws SQLException {
+    public static void deleteTransaction(Connection conn, Transaction trans) throws SQLException {
         System.out.println("Inside delete");
-        PreparedStatement statement = conn.prepareStatement("DELETE FROM `bank`.`transaction` WHERE (`idTransaction` = '"+trans.getTransactionId()+"');");
+        System.out.println("Enter id transaction you want to delete: ");
+        trans.setTransactionId(new Scanner(System.in).nextInt());
+        PreparedStatement statement = conn.prepareStatement("DELETE FROM `bank`.`transaction` WHERE (`idTransaction` = '" + trans.getTransactionId() + "');");
         int roww = statement.executeUpdate();
 
         if (roww < 0) {
             System.out.println("Delete unsuccessfull");
+        }
+    }
+
+    public static void deleteCustomer(Connection conn, Customer customer) throws SQLException {
+        System.out.println("Inside delete");
+        System.out.println("Enter id customer you want to delete: ");
+        int id;
+        customer.setCustomerID(id = new Scanner(System.in).nextInt());
+        PreparedStatement statement = conn.prepareStatement("DELETE FROM `bank`.`customer` WHERE (`idCustomer` = '" + customer.getCustomerID() + "');");
+        int roww = statement.executeUpdate();
+        if (roww < 0) {
+            System.out.println("Delete unsuccessfull");
+        }
+    }
+
+    public static void deleteAccount(Connection conn, Account account) throws SQLException {
+        System.out.println("Inside delete");
+        System.out.println("1. Delete with account id");
+        System.out.println("2. Delete with account number");
+        System.out.println();
+        switch (new Scanner(System.in).nextInt()) {
+            case 1:
+                System.out.println("Enter id account you want to delete: ");
+                account.setAccountID(new Scanner(System.in).nextInt());
+                PreparedStatement statement = conn.prepareStatement("DELETE FROM `bank`.`account` WHERE (`idAccount` = '" + account.getAccountID() + "');");
+//                int row1 = statement.executeUpdate();
+
+                if (statement.executeUpdate() < 0) {
+                    System.out.println("Delete unsuccessfull");
+                }
+                break;
+            case 2:
+                System.out.println("Enter number of account you want to delete: ");
+                account.setAccNumber(new Scanner(System.in).nextLine());
+                PreparedStatement statement2 = conn.prepareStatement("DELETE FROM `bank`.`account` WHERE (`accNumber` = '" + account.getAccNumber() + "');");
+//                int row2 = statement.executeUpdate();
+
+                if (statement2.executeUpdate() < 0) {
+                    System.out.println("Delete unsuccessfull");
+                }
+                break;
+            default:
+                System.out.println("Dont have your choice");
+                break;
         }
     }
 }
